327549e8146c8a41cf863c57988dd164a5d29832
Carson Matz <cmatz@usc.edu>
2022-06-07 23:55:53 -0400

    first stab at issues

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,11 @@ import { } from './Components/Button';
 function App() {
   return (
     <div className="App">
-      //button that goes to gdg website when clicked
-      <Button
-        onMouseOver = {() => "this.style.opacity = '0.3'"} //doesn't do anything
-        onClick = {() => window.open('https://gamedayguru.com/', '_self')}
+      <Button      //button that goes to gdg website when clicked
+        //onClick = {() => window.open('https://gamedayguru.com/', '_self')}
         variant =  'default'
         size = 'md'
+        text = "Testing Button"
       />
     </div>
   );
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,24 +1,25 @@
 import { hover } from '@testing-library/user-event/dist/hover';
 import React, {FC, useState, ReactElement} from 'react';
+import { isConstructorTypeNode } from 'typescript';
 
 export const BUTTON_CLASSNAMES : string = 
     "";
 
-// couldn't figure out how to get hover to work, click only works with inline code (don't know if that's bad practice here)
-/* export const [isHovering, setIsHovering] = useState(false);
-
-export const handleMouseEnter = () => {
-    setIsHovering(true);
+// factories
+export const mkHandleMouseEnter = (setHovered : (isHovered : boolean)=>void) => () => {
+    setHovered(true);
+};
+export const mkHandleMouseLeave = (setHovered : (isHovered : boolean)=>void) => () => {
+    setHovered(false);
 };
 
-export const handleMouseLeave = () => {
-    setIsHovering(false);
+/* export const mkOnClickAsynch = (setClick : (isClick : number)=>void) => () => {
+    setClick(isClick + 1);
 }; */
 
 //general css for button, can be altered or spoofed up
 export const BUTTON_STYLE : React.CSSProperties = {
     backgroundColor: '#33b863',
-    //opacity: isHovering ? '0' : '0.3',
     opacity: '1',
     border: '2px solid #636363',
     fontFamily: 'sans-serif',
@@ -26,60 +27,69 @@ export const BUTTON_STYLE : React.CSSProperties = {
     padding: '15px 15px',
     margin: '5px 5px',
     display: 'inline-block',
-    textAlign: 'center',
+    textAlign: 'center',   
+};
 
-    //classes for generic button sizes/types... could be implemented as well
-    //classes could be used with hover perhaps
-    /* 
-    .sm{  },
-    .md{  },
-    '&:hover': {
-        opacity: '0.3'
-    }, */
+export const LOADER_STYLE : React.CSSProperties = {
+    border: '16px solid #f3f3f3', /* Light grey */
+    border-top: '16px solid #3498db', /* Blue */
+    border-radius: '50%',
+    width: '120px',
+    height: '120px',
+    animation: 'spin 2s linear infinite',
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
 };
 
 export type ButtonProps = {
     style ? : React.CSSProperties;
-    //onMouseOver ? : () => {changeBackground()};
-    onMouseOver ? : () => void; //doesn't work
-    onClick: () => void; //takes in-line function
+   // onClick: () => void; //takes in-line function
     variant?: string; // default, submit, continue, info, exit ...
     size?: string; // sm, md, lg ...
-
-    //another unsuccesful attempt at hover
-    hover ? : {
-        opacity: '0.3'
-    };
+    text: string;
 };
-
-
-//expirementing more with hover
-/* export const onMouseOverHandler = (
-    event: React.MouseEvent<HTMLButtonElement>
-) => {
-    const Button: HTMLButtonElement = event.currentTarget;
-    BUTTON_STYLE.opacity = '0.3';
-}; */
-
-/* export function changeBackground() {
-    BUTTON_STYLE.opacity = '0.3';
-}; */
-
 
 export const Button : FC<ButtonProps>  = ({
     style,
-    onMouseOver,
-    onClick,
+   // onClick,
     variant = 'default',
     size = 'md',
+    text,
 }) =>{
 
-    return (
-        <div
+    const [isHovered, setHovered] = useState(false);
+    const [isLoading, setLoading] = useState(false);
+
+   // use your factory
+   const handleMouseEnter = mkHandleMouseEnter(setHovered);
+   const handleMouseLeave = mkHandleMouseLeave(setHovered);
+
+   toggleLoader = () => {
+       setLoading(!isLoading)
+   }
+
+   const handleClickAsync = mkOnClickAsynch(setClick);
+
+   function Normal(ButtonProps) {
+       return
+   }
+
+   return (
+        <button
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         className={BUTTON_CLASSNAMES}
-        onClick={onClick}
-        style={{...BUTTON_STYLE, ...style}}>
-            Testing
-        </div>
+        onClick={handleClickAsync}
+        style={{
+            ...BUTTON_STYLE,
+            ...style,
+            ...!isHovered ? BUTTON_STYLE : {opacity: '0.8'}
+            ...isLoading ? BUTTON_STYLE : {(LOADER_STYLE)}
+            }}>
+            {text}
+        </button>
     )
 };
